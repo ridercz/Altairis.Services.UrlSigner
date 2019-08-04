37ddb5ad440fabc91920fc2892302a290a924639
Michal Altair Valášek <michal.valasek@altairis.cz>
2019-08-04 13:56:59 +0200

    Added tests

---
+++ Altairis.UrlSigner.Tests/HmacUrlSignerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Altairis.UrlSigner.Tests {
+    public class HmacUrlSignerTest {
+        private static readonly byte[] Key = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+        private static readonly Uri TestUri = new Uri("https://www.example.com/");
+        private const string TestString = "https://www.example.com/";
+
+        [Fact]
+        public static void RoundtripUri() {
+            var signer = new HmacUrlSigner<HMACSHA512>(Key);
+            var signedUri = signer.Sign(TestUri);
+            Assert.True(signer.Verify(signedUri));
+        }
+
+        [Fact]
+        public static void RoundtripString() {
+            var signer = new HmacUrlSigner<HMACSHA512>(Key);
+            var signedUri = signer.Sign(TestString);
+            Assert.True(signer.Verify(signedUri));
+        }
+
+    }
+}
---
+++ Altairis.UrlSigner.Tests/NullUrlSignerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xunit;
+
+namespace Altairis.UrlSigner.Tests {
+    public class NullUrlSignerTests {
+        private static readonly Uri TestUri = new Uri("https://www.example.com/");
+        private const string TestString = "https://www.example.com/";
+
+        [Fact]
+        public static void SignUri() {
+            var signer = new NullUrlSigner();
+            var signedUri = signer.Sign(TestUri);
+            Assert.Equal(TestUri, signedUri);
+        }
+
+        [Fact]
+        public static void SignString() {
+            var signer = new NullUrlSigner();
+            var signedString = signer.Sign(TestString);
+            Assert.Equal(TestString, signedString);
+        }
+
+        [Fact]
+        public static void VerifyUri() {
+            var signer = new NullUrlSigner();
+            Assert.True(signer.Verify(TestUri));
+        }
+
+        [Fact]
+        public static void VerifyString() {
+            var signer = new NullUrlSigner();
+            Assert.True(signer.Verify(TestString));
+        }
+
+    }
+}

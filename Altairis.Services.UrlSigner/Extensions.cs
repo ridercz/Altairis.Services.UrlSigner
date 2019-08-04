230bdeefc854e75ea831a8b15e013ca19435db7e
Jakub Muller <muller.jakub@gmail.com>
2019-08-04 22:37:20 +0200

    added support for fragment component of url

--- Altairis.Services.UrlSigner/Extensions.cs
+++ Altairis.Services.UrlSigner/Extensions.cs
@@ -14,13 +14,38 @@ namespace Altairis.Services.UrlSigner {
             if (paramName == null) throw new ArgumentNullException(nameof(paramName));
             if (string.IsNullOrWhiteSpace(paramName)) throw new ArgumentException("Value cannot be empty or whitespace only string.", nameof(paramName));
 
-            var nameLength = paramName.Length;
+			var nameLength = paramName.Length;
 
-            var lastSeparatorIndex = url.LastIndexOfAny("?&".ToCharArray());
+			var baseUrl = url.RemoveFragment(out var fragment);
+
+			var lastSeparatorIndex = baseUrl.LastIndexOfAny("?&;".ToCharArray());
             if (lastSeparatorIndex < 1 || lastSeparatorIndex > url.Length - (nameLength + 3) || !url.Substring(lastSeparatorIndex + 1, nameLength + 1).Equals(paramName + "=", StringComparison.OrdinalIgnoreCase)) throw new FormatException("Invalid URL format");
 
-            paramValue = url.Substring(lastSeparatorIndex + nameLength + 2);
-            return url.Substring(0, lastSeparatorIndex);
+            paramValue = baseUrl.Substring(lastSeparatorIndex + nameLength + 2);
+            var shorterUrl = baseUrl.Substring(0, lastSeparatorIndex);
+
+			if (string.IsNullOrEmpty(fragment)) {
+				return shorterUrl;
+			}
+			else {
+				return shorterUrl + fragment;
+			}
         }
+
+		internal static string RemoveFragment(this string url) => RemoveFragment(url, out _);
+
+		internal static string RemoveFragment(this string url, out string fragment) {
+			if (url == null) throw new ArgumentNullException(nameof(url));
+			if (string.IsNullOrWhiteSpace(url)) throw new ArgumentException("Value cannot be empty or whitespace only string.", nameof(url));
+
+			var fragmentIndex = url.IndexOf('#');
+			if (fragmentIndex < 0) {
+				fragment = null;
+				return url;
+			}
+
+			fragment = url.Substring(fragmentIndex);
+			return url.Substring(0, fragmentIndex);
+		}
     }
 }
